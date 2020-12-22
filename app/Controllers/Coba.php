f629121906e403df8dd54ce74e4cb198b8d096e6
Rizuki Ryumada <rizki3366@msn.cn>
2020-12-22 22:06:37 +0700

    Update Coba.php

--- app/Controllers/Coba.php
+++ app/Controllers/Coba.php
@@ -5,11 +5,15 @@ use CodeIgniter\Controller;
 
 class Coba extends BaseController{
     public function index(){
+        
+        
         echo "ini controller index";
     }
 
     public function about(){
-        echo "Hai, nama gua $this->nama";
+        redirect('pages','refresh');
+
+        // echo "Hai, nama gua $this->nama";
     }
 
     public function panggil($nama = "", $pesan = ""){ // kasih default
